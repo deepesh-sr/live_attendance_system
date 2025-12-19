f7207631ce2bbb9116e0f0866c063573938ed452
deepesh-sr <rathoredeepeshsingh@gmail.com>
2025-12-19 16:24:37 +0530

    Add : Express server initialised.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello Deepesh")
+import express from 'express'
+
+const app = express();
+
+app.use(express.json());
+app.get('/health',(req,res)=>{
+    console.log("Hello");
+    res.send("Helloooooo")
+})
+
+app.listen(3000,()=>{
+    console.log("App is listening of port 3000")
+})
