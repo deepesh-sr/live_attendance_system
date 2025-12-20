b551632f5089c4de05f57afcbaa6e229792790e4
deepesh-sr <220714100078@cutm.ac.in>
2025-12-20 13:22:01 +0530

    Add : Signup, password Hashing.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import express from 'express'
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose';
+import * as zod from 'zod'
+import { User } from './database/model.js';
+import bcrypt from 'bcrypt'
 
 dotenv.config();
 
@@ -9,11 +12,11 @@ const app = express();
 
 app.use(express.json());
 
-async function connectDB(){
+async function connectDB() {
 
     try {
-        const monogodb_url= process.env.DB_URL;
-        if(!monogodb_url) {
+        const monogodb_url = process.env.DB_URL;
+        if (!monogodb_url) {
             throw new Error("DB URL must be provided")
         }
         await mongoose.connect(monogodb_url);
@@ -23,14 +26,66 @@ async function connectDB(){
         console.error("Error connecting with database", error);
     }
 }
-
 connectDB();
+
+//zod validation 
+const user = zod.object({
+    name: zod.string,
+    email: zod.string,
+    password: zod.string()
+        .min(8)
+        .max(20)
+        .refine((password) => /[A-Z]/.test(password))
+        .refine((password) => /[a-z]/.test(password))
+        .refine((p) => /[0-9]/.test(p))
+        .refine((p) => /[!@#$%^&*]/.test(p)),
+    role: zod.enum(["teacher", "student"])
+
+})
 app.get('/health', (req, res) => {
     console.log("Hello");
     res.send("Helloooooo")
 })
+app.post('/signup', async (req, res) => {
+    try {
+        const result = user.safeParse(req.body);
+        if (result.success) {
+            const { username, email, password, role } = req.body;
+            const userdata = await User.findOne({
+                username: username,
+                email: email
+            });
 
+            if (userdata) {
+                res.status(403).json({
+                    msg: "User already exist"
+                })
+            }
 
+            const hashedPassword = bcrypt.hash(req.body.password, 2);
+
+            const newUser = new User({
+                username: username,
+                email: email,
+                password: hashedPassword,
+                role: role
+            })
+            const saveResult = await newUser.save();
+            if (saveResult) {
+                res.status(200).json({ "msg": "Signed up" })
+            }
+        } else {
+            console.error(result.error);
+            res.status(411).json({
+                message: "Invalid Inputs"
+            })
+        }
+    } catch (error) {
+        res.json(500).json({
+            msg: "server Error"
+        })
+    }
+})
 
 app.post('/signin', (req, res) => {
     const { username, password } = req.body;
