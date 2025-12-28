29acb032987e8e529c9d7cbdb34d5963e8b0b69f
deepesh-sr <220714100078@cutm.ac.in>
2025-12-28 16:13:24 +0530

    Add : Student auth middleware.

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -61,3 +61,38 @@ export const authenticateTeacher = async (req: Request, res: Response, next: Nex
         })
     }
 }
+
+export const authenticateStudent = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+        const token = req.headers['authorization'];
+
+        if (!token) {
+            res.status(401).json({
+                msg: "Token must be provided."
+            })
+        }
+        const jwtSecret = process.env.JWT_SECRET;
+        if (token && jwtSecret) {
+            const decodedMessage = jwt.verify(token, jwtSecret)
+            // console.log(decodedMessage)
+            //   @ts-ignore
+            req.role = decodedMessage.role;
+            //@ts-ignore
+            if ( req.role == 'student'){
+                next();
+            }
+            else { 
+                res.status(401).json({
+                    msg : "Authorization Error. Must be a student."
+                })
+            }
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(401).json({
+            msg : "Invalid or expired token."
+        })
+    }
+}
